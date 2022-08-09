03b2565bc7e951d6486b268d4e5386d68394df59
Kasun Kariyawasam <kasunsk@gmail.com>
2022-08-09 11:09:48 +0800

    rest GET implimentation

--- src/main/kotlin/com/example/kotlinweb/KotlinWebApplication.kt
+++ src/main/kotlin/com/example/kotlinweb/KotlinWebApplication.kt
@@ -2,10 +2,25 @@ package com.example.kotlinweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinWebApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlinWebApplication>(*args)
+    runApplication<KotlinWebApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+            Message("1", "Hello!"),
+            Message("2", "Bonjour!"),
+            Message("3", "Privet!"),
+    )
+}
+
+data class Message(val id: String?, val text: String)
